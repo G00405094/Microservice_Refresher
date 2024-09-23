609786037919a40320b0f9360c87b9ebe6ff5241
G00405094@atu.ie <G00405094@atu.ie>
2024-09-23 16:18:54 +0100

    Set up

---
+++ src/main/java/com/example/microservicerefresher/MicroserviceRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicerefresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRefresherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceRefresherApplication.class, args);
+    }
+
+}
