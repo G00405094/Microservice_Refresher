5ad7f1b3d7d0af7dee051b025f00aa2fd835b224
G00405094@atu.ie <G00405094@atu.ie>
2024-09-23 16:28:22 +0100

    Set up

--- src/main/java/com/example/microservicerefresher/MicroserviceRefresher.java
+++ src/main/java/com/example/microservicerefresher/MicroserviceRefresher.java
@@ -1,6 +1,7 @@
 package com.example.microservicerefresher;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class MicroserviceRefresher {
     public String message()
     {
         return "Hello";
+    }
+    @GetMapping("/hello/{name}")
+    public String myName(@PathVariable String name)
+    {
+        return "Your Name is " + name;
     }
 }
