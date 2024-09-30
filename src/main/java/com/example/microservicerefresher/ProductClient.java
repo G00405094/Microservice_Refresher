487b41a21f0c270b56042aaaaa9774937a765c43
G00405094@atu.ie <G00405094@atu.ie>
2024-09-30 17:40:11 +0100

    Week 3 update

--- src/main/java/com/example/microservicerefresher/ProductClient.java
+++ src/main/java/com/example/microservicerefresher/ProductClient.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "external-service", url = "http://localhost:8081")
 public interface ProductClient {
-    @GetMapping
+
 }
